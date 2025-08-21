6650b986ae80ec83a1ef35b1252b3ca43f7e1fb0
Siarhei B <sergeybednyuk@gmail.com>
2025-08-21 17:31:29 +0200

    Implement Update user profile functionality and some refactoring

---
+++ Trinder.UserProfile.Application/TrinderUserProfile/Commands/UpdateUserProfileInfo/UpdateUserProfileInfoCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Trinder.UserProfile.Application.TrinderUserProfile.Commands.UpdateUserProfile;
+
+public class UpdateUserProfileInfoCommand(int id, string userName, string email, string? bio = null) : IRequest<bool>
+{
+    public int Id { get; } = id;
+    public string UserName { get; } = userName;
+    public string Email { get; } = email;
+    public string? Bio { get; } = bio;
+}
---
+++ Trinder.UserProfile.Application/TrinderUserProfile/Commands/UpdateUserProfileInfo/UpdateUserProfileInfoCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Trinder.UserProfile.Application.TrinderUserProfile.Commands.UpdateUserProfile;
+using Trinder.UserProfile.Domain.RepositoriesInterfaces;
+
+namespace Trinder.UserProfile.Application.TrinderUserProfile.Commands.UpdateUserProfileInfo;
+
+public class UpdateUserProfileInfoCommandHandler(ILogger<UpdateUserProfileInfoCommandHandler> logger, 
+    IUserProfilesRepository userProfilesRepository) : IRequestHandler<UpdateUserProfileInfoCommand, bool>
+{
+    public async Task<bool> Handle(UpdateUserProfileInfoCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Updating user profile with UserName: {UserName} and Email: {Email}",
+                        request.UserName, request.Email);
+
+        var user = await userProfilesRepository.GetByIdAsync(request.Id, cancellationToken);
+        if (user is null) throw new Exception($"User profile with {request.Id} id doesn't exists");
+
+        var userProfileToBeUpdated = new Domain.Entities.TrinderUserProfile
+        {
+            Id = request.Id,
+            UserEmail = request.Email,
+            UserName = request.UserName,
+            Bio = request.Bio
+        };
+
+        var result = await userProfilesRepository.UpdateAsync(userProfileToBeUpdated, cancellationToken);
+
+        return result;
+    }
+}
