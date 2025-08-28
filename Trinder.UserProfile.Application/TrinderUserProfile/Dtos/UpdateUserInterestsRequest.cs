420443301d56fd6555e8f1d9d14f5a4a3474065b
Siarhei B <sergeybednyuk@gmail.com>
2025-08-28 19:17:12 +0200

    complete AddUserProfileInterests implementation

---
+++ Trinder.UserProfile.Application/TrinderUserProfile/Dtos/UpdateUserInterestsRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Trinder.UserProfile.Application.TrinderUserProfile.Dtos;
+
+public class UpdateUserInterestsRequest
+{
+    public ICollection<int> InterestIds { get; set; } = default!;
+}
